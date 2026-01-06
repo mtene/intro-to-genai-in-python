41df3c6d3067d38ba81932c2dc6c38527c484d51
Matei Tene <matei.tene@gmail.com>
2026-01-06 15:31:15 +0100

    Logging configuration error control

--- src/chatbot/utils/logging.py
+++ src/chatbot/utils/logging.py
@@ -1,9 +1,21 @@
 import logging
+from typing import List
 from rich.logging import RichHandler
 from chatbot.config import config
 
 
 def configure_logging() -> None:
+    errors: List[str] = []
+
+    # get logging level from config
+    config_log_level = config.get_log_level()
+    log_level = logging.getLevelNamesMapping().get(config_log_level.upper())
+    if log_level is None:
+        log_level = logging.INFO
+        errors.append(
+            f"Invalid logging level '{config_log_level}', using '{logging.getLevelName(log_level).lower()}'"
+        )
+
     # setup console log coloring handler
     handler = RichHandler(
         rich_tracebacks=True,  # Enable rich tracebacks
@@ -12,9 +24,16 @@ def configure_logging() -> None:
         show_path=False,  # Disable file path display
     )
 
-    level = logging.getLevelNamesMapping().get(config.get_log_level().upper())
-    logging.basicConfig(level=level, format="%(message)s", handlers=[handler])
+    # configure logging
+    logging.basicConfig(level=log_level, format="%(message)s", handlers=[handler])
 
     # suppress some the 3rd party packages INFO logging, to reduce clutter
-    for logger_name in ["httpx", "mcp"]:
-        logging.getLogger(logger_name).setLevel(logging.ERROR)
+    if log_level > logging.DEBUG:
+        for logger_name in ["httpx", "mcp"]:
+            logging.getLogger(logger_name).setLevel(logging.WARNING)
+
+    # now that logging is setup, report any errors encountered
+    if errors:
+        logger = logging.getLogger(__name__)
+        for error in errors:
+            logger.error(error)
